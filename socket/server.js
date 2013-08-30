c65de8b23c372ce40c0c5f265b02d9a1c04f1fa8
Gian Giovani <wejick@gmail.com>
2013-08-30 09:14:27 +0700

    TEST

--- socket/server.js
+++ socket/server.js
@@ -120,41 +120,7 @@ WHERE host.active = 1 AND file.id_file = " + fileId;
 			// info array of data : 1 id_file, 2 avail_block
 			// gimana kalau ada file yang terhapus/ sudah tak tersedia?
 
-			function nodeUpdate(info) {
-				// 0 UN | 1 Filename | 2 block_avail | 3 id_host | 4 id_file
-
-				db.serialize(function () {
-					console.log(info[3] + " " + socket.remoteAddress);
-					db.all("SELECT id_file, id_host FROM file_host_rel where id_file = " + info[4] + " AND id_host = " + info[3],
-						function (err, row) {
-							if (row == undefined || row.length < 1) {
-								var query = "INSERT INTO file_host_rel(id_file, id_host,block_avail) VALUES (" + info[4] + "," + info[3] + "," + info[2] + ")"
-								console.log(query);
-								db.all(query,
-									function (err, row) {
-										if (err)
-											console.log('');
-									}
-								);
-							} else {
-								var query = "UPDATE file_host_rel SET block_avail = " + info[2] + " WHERE id_file = " + info[4] + " AND id_host= " + info[3];
-								console.log(query);
-								db.all(query,
-									function (err, row) {
-										if (err)
-											console.log('');
-									}
-								);
-							}
-						}
-					);
-				});
-			}
-			function nodeUpdateName(info) {
-				// write request to console
-			for (var i = 0; i < info.length; i++) {
-				console.log(" UNInfo[" + i + "] : " + info[i]);
-			}
+			function nodeUpdate(info) {	
 				db.serialize(function () {
 					console.log(info[3] + " " + socket.remoteAddress);
 					db.all("SELECT id_file, id_host FROM file_host_rel where id_file = " + info[1] + " AND id_host = " + info[3],
@@ -170,6 +136,39 @@ WHERE host.active = 1 AND file.id_file = " + fileId;
 								);
 							} else {
 								var query = "UPDATE file_host_rel SET block_avail = " + info[2] + " WHERE id_file = " + info[1] + " AND id_host= " + info[3];
+								console.log(query);
+								db.all(query,
+									function (err, row) {
+										if (err)
+											console.log('');
+									}
+								);
+							}
+						}
+					);
+				});
+			}
+			function nodeUpdateName(info) {
+			// 0 UN | 1 Filename | 2 block_avail | 3 id_host | 4 id_file
+			// write request to console
+			for (var i = 0; i < info.length; i++) {
+				console.log(" UNInfo[" + i + "] : " + info[i]);
+			}
+				db.serialize(function () {
+					console.log(info[3] + " " + socket.remoteAddress);
+					db.all("SELECT id_file, id_host FROM file_host_rel where id_file = " + info[4] + " AND id_host = " + info[3],
+						function (err, row) {
+							if (row == undefined || row.length < 1) {
+								var query = "INSERT INTO file_host_rel(id_file, id_host,block_avail) VALUES (" + info[4] + "," + info[3] + "," + info[2] + ")"
+								console.log(query);
+								db.all(query,
+									function (err, row) {
+										if (err)
+											console.log('');
+									}
+								);
+							} else {
+								var query = "UPDATE file_host_rel SET block_avail = " + info[2] + " WHERE id_file = " + info[4] + " AND id_host= " + info[3];
 								console.log(query);
 								db.all(query,
 									function (err, row) {
